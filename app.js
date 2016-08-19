da7493b4aeaa40ec72f6c360dfb78d4e71deb0e1
caraldi <araldicm@Caitlin-M-Araldi.local>
2016-08-19 16:01:58 -0700

    sum array solved

--- app.js
+++ app.js
@@ -85,7 +85,7 @@ console.log('Test array:', testArray);
 function sumArray(testArray){ //eslint-disable-line
   var arraySum = sum(sum(testArray[0], testArray[1])[0], testArray[2])[0];
   console.log('Sum of array:', arraySum);
-  var arrayMessage = 2 + ',' + 3 + ',' + 4 + ' was passed in as an array of numbers, and ' + arraySum + ' is their sum.';
+  var arrayMessage = testArray[0] + ',' + testArray [1] + ',' + testArray[2] + ' was passed in as an array of numbers, and ' + arraySum + ' is their sum.';
   console.log('Array message:', arrayMessage);
   return [arraySum, arrayMessage];
 }
@@ -105,13 +105,17 @@ Write a function called multiplyArray() that takes an array of numbers as its ar
 IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this.
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
-
+console.log('Test array:', testArray);
 // Write your code here
 function multiplyArray(testArray){ //eslint-disable-line
-
+  var arrayProduct = multiply(multiply(testArray[0], testArray[1])[0], testArray[2])[0];
+  console.log('Array product:', arrayProduct);
+  var arrayMessage = 'The numbers ' + testArray[0] + ',' + testArray[1] + ',' + testArray[2] + ' have a product of ' + arrayProduct + '.';
+  console.log('Array message:', arrayMessage);
+  return [arrayProduct, arrayMessage];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(2,3,4);
+testMultiplyArray(2,3,4);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
