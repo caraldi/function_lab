ea55c111cdc2301abd6e46cb6d469b8b06226607
caraldi <araldicm@Caitlin-M-Araldi.local>
2016-08-19 16:17:22 -0700

    multiply array solved

--- app.js
+++ app.js
@@ -105,13 +105,15 @@ Write a function called multiplyArray() that takes an array of numbers as its ar
 IMPORTANT DETAIL: You may not use the arithmetic operator * in this function. To do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this.
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiplyArray() function and see if the test passes.*/
-
+console.log('Test array:', testArray);
 // Write your code here
 function multiplyArray(testArray){ //eslint-disable-line
-
+  var arrayProduct = multiply(multiply(testArray[0], testArray[1])[0], testArray[2])[0];
+  console.log('Array product:', arrayProduct);
+  var arrayMessage = 'The numbers ' + testArray[0] + ',' + testArray[1] + ',' + testArray[2] + ' have a product of ' + arrayProduct + '.';
+  console.log('Array message:', arrayMessage);
+  return [arrayProduct, arrayMessage];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(2,3,4);
-
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
+testMultiplyArray(2,3,4);
